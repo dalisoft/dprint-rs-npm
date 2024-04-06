4574f73c3600372ce77f6c2f582eaece0e8717d0
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-07 04:22:41 +0500

    fix: upgrade dependencies and upgrade install method

--- prepare.js
+++ prepare.js
@@ -1,10 +1,12 @@
 import { prepare, maps } from "binary2npm";
 
+maps.os.linux = "linux-gnu";
+
 await prepare({
   remote: "github",
   author: "dprint",
   repository: "dprint",
   remoteToken: process.env.GITHUB_TOKEN,
   binary: "dprint",
-  useVersion: false,
+  orders: ["binary", "arch", "vendor", "os"],
 });
